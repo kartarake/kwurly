48ae7a1a97772c9af661e08edba7edbeca354930
kartarake <kar.prahveen@gmail.com>
2025-01-21 21:23:00 +0530

    Textbox GUI

--- kwurly/lib/main.dart
+++ kwurly/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    windowManager.maximize();
     return MaterialApp(
       title:"kwurly",
       debugShowCheckedModeBanner: false,
@@ -22,11 +26,98 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomePage"),
+        actions: actionlist,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text("Text"),
-      ),
+
+      body: Scaffold(
+        body: Center(
+          child: getinputbox() 
+        )
+      )
     );
   }
+}
+
+var actionlist = [
+  Padding(
+    padding: const EdgeInsets.only(right: 15),
+    child: IconButton(
+      icon: SvgPicture.asset(
+        "assets\\icons\\tabler--settings.svg",
+        width: 24,
+        height: 24,
+      ),
+      onPressed: () {},
+    ),
+  ),
+];
+
+Container getinputbox() {
+  return Container(
+    width: 1209,
+    height: 177+34,
+
+    decoration: BoxDecoration(
+      border: Border.all(
+        color: Color(0xff191919),
+        width: 0.5,
+      ),
+      borderRadius: BorderRadius.circular(20)
+    ),
+    
+    child: Column(
+      children: [
+        TextField(
+          style: TextStyle(
+            fontFamily: "Comic",
+            fontSize: 18,
+          ),
+          maxLines: 4,
+          decoration: InputDecoration(
+            hintText: 'Your idea goes here ..',
+            hintFadeDuration: Duration(milliseconds: 500),
+            hintStyle: TextStyle(
+              color: Colors.grey
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 30
+            ),
+            border: InputBorder.none
+          ),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () {}, 
+              icon: SvgPicture.asset(
+                "assets\\icons\\tabler--device-floppy.svg",
+                width: 24,
+                height: 24,
+              )
+            ),
+
+            SizedBox(width: 5),
+
+            IconButton(
+              onPressed: () {}, 
+              icon: SvgPicture.asset(
+                "assets\\icons\\tabler--eraser.svg",
+                width: 24,
+                height: 24,
+              )
+            ),
+
+            SizedBox(width: 20)
+          ],
+        ),
+
+        SizedBox(height: 10)
+      ]      
+    )
+  );
+
 }
